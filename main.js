2bf088fa5d8a0e95dd32f0fc3006a9c5ebf49ace
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-10-31 20:25:27 -0600

    using JSON parse on JSON now, hoping to use the each method to recursively grab tags and content

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   // Check for the various File API support.
   if (window.File && window.FileReader && window.FileList) {
-    
+
   } else {
     alert('The File APIs are not fully supported in this browser.');
   }
@@ -13,20 +13,47 @@ $(document).ready(() => {
       renderJSON(newFile);
   });
 
-
   function renderJSON(file) {
 
     // generate a new FileReader object
     var reader = new FileReader();
 
+    if (file.type !== 'application/json') {
+      alert('Please input a valid JSON file.');
+    } else {
+      //fires onload function below once the file is read
+      reader.readAsText(file);
+    }
+
+
     reader.onload = function(event) {
 
       const loadedJSON = event.target.result;
-      console.log(loadedJSON);
+
+      let convertedJSON = JSON.parse(loadedJSON);
+
+      jsonToHtml(convertedJSON);
+
     };
 
-    // when the file is read it triggers the onload event above.
-    reader.readAsText(file);
+  }
+
+  function jsonToHtml(json) {
+    $(json).each(function(index, element) {
+      console.log(element.tag, 'tag!');
+      console.log(element.content);
+
+      jsonToHtml(element.content);
+
+      // if (typeof element.content === Object) {
+      //   console.log('got inside conditional');
+      //   console.log(element.content, 'content!');
+      //   jsonToHtml(element.content);
+      //   // jsonToHtml(element.content);
+      //
+      // }
+    });
+
   }
 
 });
